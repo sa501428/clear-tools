bca75700106d5feaf3e6c3645c845287c3377449
Muhammad S Shamim <sa501428@gmail.com>
2022-03-11 02:51:01 -0600

    parameter for score filtering

--- src/flags/Main.java
+++ src/flags/Main.java
@@ -5,6 +5,6 @@ public class Main {
     public static void main(String[] args) {
         // 0 filename
         // 1 bed file
-        AVD avd = new AVD(args[0], args[1]);
+        AVD avd = new AVD(args[0], args[1], Integer.parseInt(args[2]));
     }
 }
