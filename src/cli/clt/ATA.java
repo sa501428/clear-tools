a94c9003deedc3f6413e9b428a6aa24437ff59a5
Muhammad Saad Shamim <sa501428@gmail.com>
2022-06-14 14:34:00 -0500

    update ATA reading

--- src/cli/clt/ATA.java
+++ src/cli/clt/ATA.java
@@ -1,25 +1,23 @@
 package cli.clt;
 
 import cli.Main;
+import cli.utils.IGVUtils;
 import javastraw.reader.basics.Chromosome;
 import javastraw.reader.basics.ChromosomeHandler;
 import javastraw.reader.basics.ChromosomeTools;
 import org.broad.igv.feature.IGVFeature;
 import org.broad.igv.feature.LocusScore;
-import org.broad.igv.feature.genome.Genome;
 import org.broad.igv.feature.tribble.TribbleIndexNotFoundException;
-import org.broad.igv.track.*;
-import org.broad.igv.util.ResourceLocator;
+import org.broad.igv.track.DataTrack;
+import org.broad.igv.track.TribbleFeatureSource;
+import org.broad.igv.track.WindowFunction;
 
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Iterator;
 import java.util.List;
 
 public class ATA {
 
-    private final static double log2 = Math.log(2.0D);
 
     public static void run(String[] args, int resolution, boolean exportNPY) {
         if (args.length != 5) {
@@ -35,92 +33,34 @@ public class ATA {
 
         ChromosomeHandler handler = ChromosomeTools.loadChromosomes(genome);
 
-        aggregate(inputBigWig, bedFile, handler, resolution, outFile, genome);
+        try {
+            aggregate(inputBigWig, bedFile, handler, resolution, outFile, genome);
+        } catch (IOException | TribbleIndexNotFoundException e) {
+            e.printStackTrace();
+            throw new RuntimeException(e);
+        }
     }
 
-    public static void main(String[] args) throws IOException, TribbleIndexNotFoundException {
-        String inputBed = "https://www.encodeproject.org/files/ENCFF073ORT/@@download/ENCFF073ORT.bed.gz";
-        ResourceLocator locator = new ResourceLocator(inputBed);
+    private static void aggregate(String inputBigWig, String bedFile, ChromosomeHandler handler, int resolution, String outFile, String genomeID) throws IOException, TribbleIndexNotFoundException {
+        DataTrack igvTrack = IGVUtils.loadBigWig(inputBigWig, handler);
 
-        String genomeID = "hg38";
-        ChromosomeHandler handler = ChromosomeTools.loadChromosomes(genomeID);
-        List<Chromosome> chrList = new ArrayList<>(Arrays.asList(handler.getChromosomeArray()));
-        List<org.broad.igv.feature.Chromosome> igvChrList = new ArrayList<>();
-        for (Chromosome chrom : chrList) {
-            igvChrList.add(chrom.toIGVChromosome());
+        Chromosome chrom = handler.getChromosomeFromName("chr1");
+
+        List<LocusScore> loci = IGVUtils.getLocusScores(igvTrack, chrom, 0, 100, resolution,
+                WindowFunction.count);
+        for (LocusScore locus : loci) {
+            System.out.println(locus.getStart() + " " + locus.getEnd() + " " + locus.getScore());
         }
 
-        Genome genome = new Genome(genomeID, igvChrList);
 
-        TribbleFeatureSource src = TribbleFeatureSource.getFeatureSource(locator, genome);
+        TribbleFeatureSource src = IGVUtils.loadBed(bedFile, handler);
 
-        Iterator<?> iter = src.getFeatures("chr1", 180000, 182000);
+        Iterator<?> iter = src.getFeatures("chr10", 0, (int) handler.getChromosomeFromName("chr10").getLength());
         while (iter.hasNext()) {
             IGVFeature feature = (IGVFeature) iter.next();
             System.out.println(feature.getStart() + " " + feature.getEnd() + " " + feature.getScore());
         }
-    }
-
-    public static void main2(String[] args) {
-
-        String inputBigWig = "https://s3.us-central-1.wasabisys.com/aiden-encode-hic-mirror/loops/S5_PC3_signal.bw";
-        String genomeID = "hg38";
-        ChromosomeHandler handler = ChromosomeTools.loadChromosomes(genomeID);
-        List<Chromosome> chrList = new ArrayList<>(Arrays.asList(handler.getChromosomeArray()));
-        List<org.broad.igv.feature.Chromosome> igvChrList = new ArrayList<>();
-        for (Chromosome chrom : chrList) {
-            igvChrList.add(chrom.toIGVChromosome());
-        }
-
-        Genome genome = new Genome(genomeID, igvChrList);
-        ResourceLocator locator = new ResourceLocator(inputBigWig);
-
-        List<Track> tracks = (new TrackLoader()).load(locator, genome);
-        DataTrack igvTrack = (DataTrack) tracks.get(0);
-
-        Chromosome chrom = handler.getChromosomeFromName("chr1");
-        int res = 1;
-        int binCount = (int) (chrom.getLength() / res + 1);
-        int zoom = Math.max(0, (int) (Math.log(binCount / 700f) / log2));
-
-        List<LocusScore> loci = getLocusScores(igvTrack, "chr1", 10000, 11000, zoom,
-                WindowFunction.max);
-
-        for (LocusScore locus : loci) {
-            System.out.println(locus.getStart() + " " + locus.getEnd() + " " + locus.getScore());
-        }
-    }
-
-    private static void aggregate(String inputBigWig, String bedFile, ChromosomeHandler handler, int resolution, String outFile, String genomeID) {
-        List<Chromosome> chrList = new ArrayList<>(Arrays.asList(handler.getChromosomeArray()));
-        List<org.broad.igv.feature.Chromosome> igvChrList = new ArrayList<>();
-        for (Chromosome chrom : chrList) {
-            igvChrList.add(chrom.toIGVChromosome());
-        }
-
-        Genome genome = new Genome(genomeID, igvChrList);
-        ResourceLocator locator = new ResourceLocator(inputBigWig);
-
-        List<Track> tracks = (new TrackLoader()).load(locator, genome);
-        DataTrack igvTrack = (DataTrack) tracks.get(0);
-
-        List<LocusScore> loci = getLocusScores(igvTrack, "chr1", 0, 100, 1000000,
-                WindowFunction.mean);
-
-        for (LocusScore locus : loci) {
-            System.out.println(locus.getStart() + " " + locus.getEnd() + " " + locus.getScore());
-        }
 
 
     }
-
-    protected static List<LocusScore> getLocusScores(DataTrack igvTrack, String chr, long gStart, long gEnd, int zoom,
-                                                     WindowFunction windowFunction) {
-        igvTrack.setWindowFunction(windowFunction);
-        org.broad.igv.track.LoadedDataInterval<List<LocusScore>> scores = igvTrack.getSummaryScores(chr, (int) gStart, (int) gEnd, zoom);
-        // Problems with human not having the "chr".  Return scores if not 0, otherwise try adding "chr"
-        if (scores.getFeatures().size() > 0) return scores.getFeatures();
-        else return igvTrack.getSummaryScores("chr" + chr, (int) gStart, (int) gEnd, zoom).getFeatures();
-    }
-
 }
---
+++ src/cli/utils/IGVUtils.java
@@ -0,0 +1,60 @@
+package cli.utils;
+
+import javastraw.reader.basics.Chromosome;
+import javastraw.reader.basics.ChromosomeHandler;
+import org.broad.igv.feature.LocusScore;
+import org.broad.igv.feature.genome.Genome;
+import org.broad.igv.feature.tribble.TribbleIndexNotFoundException;
+import org.broad.igv.track.*;
+import org.broad.igv.util.ResourceLocator;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class IGVUtils {
+    private final static double log2 = Math.log(2.0D);
+
+    public static List<LocusScore> getLocusScores(DataTrack igvTrack, Chromosome chromosome, long gStart, long gEnd, int resolution,
+                                                  WindowFunction windowFunction) {
+        int binCount = (int) (chromosome.getLength() / resolution + 1);
+        int zoom = Math.max(0, (int) (Math.log(binCount / 700f) / log2));
+
+        igvTrack.setWindowFunction(windowFunction);
+        LoadedDataInterval<List<LocusScore>> scores = igvTrack.getSummaryScores(chromosome.getName(), (int) gStart, (int) gEnd, zoom);
+        // Problems with human not having the "chr".  Return scores if not 0, otherwise try adding "chr"
+        if (scores.getFeatures().size() > 0) return scores.getFeatures();
+        else
+            return igvTrack.getSummaryScores("chr" + chromosome.getName(), (int) gStart, (int) gEnd, zoom).getFeatures();
+    }
+
+    public static DataTrack loadBigWig(String inputBigWig, ChromosomeHandler handler) {
+
+        List<Chromosome> chrList = new ArrayList<>(Arrays.asList(handler.getChromosomeArray()));
+        List<org.broad.igv.feature.Chromosome> igvChrList = new ArrayList<>();
+        for (Chromosome chrom : chrList) {
+            igvChrList.add(chrom.toIGVChromosome());
+        }
+
+        Genome genome = new Genome(handler.getGenomeID(), igvChrList);
+        ResourceLocator locator = new ResourceLocator(inputBigWig);
+
+        List<Track> tracks = (new TrackLoader()).load(locator, genome);
+        return (DataTrack) tracks.get(0);
+    }
+
+    public static TribbleFeatureSource loadBed(String inputBed, ChromosomeHandler handler) throws IOException, TribbleIndexNotFoundException {
+        ResourceLocator locator = new ResourceLocator(inputBed);
+
+        List<Chromosome> chrList = new ArrayList<>(Arrays.asList(handler.getChromosomeArray()));
+        List<org.broad.igv.feature.Chromosome> igvChrList = new ArrayList<>();
+        for (Chromosome chrom : chrList) {
+            igvChrList.add(chrom.toIGVChromosome());
+        }
+
+        Genome genome = new Genome(handler.getGenomeID(), igvChrList);
+
+        return TribbleFeatureSource.getFeatureSource(locator, genome);
+    }
+}
