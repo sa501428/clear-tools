5f2c0218eee7ef48ba04487f10f01258261b73c9
Muhammad Saad Shamim <sa501428@gmail.com>
2022-07-07 00:35:00 -0500

    reorganize

--- src/cli/clt/Sift.java
+++ src/cli/clt/Sift.java
@@ -4,10 +4,7 @@ import cli.Main;
 import cli.utils.ExpectedUtils;
 import cli.utils.VectorCleanerUtils;
 import cli.utils.WelfordStats;
-import cli.utils.sift.SiftUtils;
-import cli.utils.sift.SimpleLocation;
-import cli.utils.sift.Z4Scores;
-import cli.utils.sift.ZScores;
+import cli.utils.sift.*;
 import javastraw.feature2D.Feature2D;
 import javastraw.feature2D.Feature2DList;
 import javastraw.reader.Dataset;
@@ -234,7 +231,7 @@ public class Sift {
                     VectorCleanerUtils.inPlaceClean(vector1);
                     VectorCleanerUtils.inPlaceClean(vector1b);
 
-                    inPlaceFilterByNorms(initialPoints, vector1, vector1b, lowRes / hires);
+                    CoverageFiltering.inPlaceFilterByNorms(initialPoints, vector1, vector1b, lowRes / hires);
 
                     System.out.println("Num initial loops after filter0 " + initialPoints.size());
 
@@ -244,7 +241,7 @@ public class Sift {
                     Set<SimpleLocation> enrichedRegions = getExtremeLocations(ds, chrom.getIndex(), lowRes,
                             zd1, MAX_DIST / lowRes, MIN_DIST / lowRes);
 
-                    filterOutByOverlap(initialPoints, enrichedRegions, lowRes / hires);
+                    NMSUtils.filterOutByOverlap(initialPoints, enrichedRegions, lowRes / hires);
                     enrichedRegions.clear();
                     System.out.println("LowRes pass done (" + lowRes + ")");
 
@@ -271,70 +268,6 @@ public class Sift {
         return output;
     }
 
-    private void inPlaceFilterByNorms(Set<ContactRecord> initialPoints, double[] vector1, double[] vector1b, int scalar) {
-        List<ContactRecord> toDelete = new LinkedList<>();
-        for (ContactRecord record : initialPoints) {
-            if (!normsAreGood(record, vector1, vector1b, scalar)) {
-                toDelete.add(record);
-            }
-        }
-        toDelete.forEach(initialPoints::remove);
-    }
-
-    private boolean normsAreGood(ContactRecord record, double[] vector1, double[] vector2, int scalar) {
-        return normIsGood(vector1, record, scalar) && normIsGood(vector2, record, scalar);
-    }
-
-    private boolean normIsGood(double[] vector, ContactRecord record, int scalar) {
-        return vector[record.getBinX() / scalar] > 1 && vector[record.getBinY() / scalar] > 1;
-    }
-
-    private void filterOutByOverlap(Set<ContactRecord> initialPoints, int scalar) {
-        Set<ContactRecord> toRemove = new HashSet<>();
-        Map<SimpleLocation, List<ContactRecord>> locationMap = new HashMap<>();
-        for (ContactRecord cr : initialPoints) {
-            SimpleLocation region = new SimpleLocation(cr.getBinX() / scalar, cr.getBinY() / scalar);
-            if (locationMap.containsKey(region)) {
-                locationMap.get(region).add(cr);
-            } else {
-                List<ContactRecord> values = new ArrayList<>();
-                values.add(cr);
-                locationMap.put(region, values);
-            }
-        }
-
-        nonMaxSuppressionInGroup(initialPoints, toRemove, locationMap);
-    }
-
-    private void nonMaxSuppressionInGroup(Set<ContactRecord> initialPoints, Set<ContactRecord> toRemove, Map<SimpleLocation, List<ContactRecord>> locationMap) {
-        for (List<ContactRecord> overlaps : locationMap.values()) {
-            if (overlaps.size() > 1) {
-                float max = getMax(overlaps);
-                for (ContactRecord cr2 : overlaps) {
-                    if (cr2.getCounts() < max) {
-                        toRemove.add(cr2);
-                    }
-                }
-            }
-        }
-        initialPoints.removeAll(toRemove);
-        locationMap.clear();
-    }
-
-    private float getMax(List<ContactRecord> records) {
-        float maxVal = records.get(0).getCounts();
-        for (ContactRecord cr : records) {
-            if (cr.getCounts() > maxVal) {
-                maxVal = cr.getCounts();
-            }
-        }
-        return maxVal;
-    }
-
-    private boolean inRegions(ContactRecord cr, Set<SimpleLocation> regions, int scalar) {
-        SimpleLocation region = new SimpleLocation(cr.getBinX() / scalar, cr.getBinY() / scalar);
-        return regions.contains(region);
-    }
 
     private static int logp1i(int x) {
         return (int) Math.floor(Math.log(1 + x));
@@ -344,24 +277,4 @@ public class Sift {
         return Math.log(1 + x);
     }
 
-    private void filterOutByOverlap(Set<ContactRecord> initialPoints, Set<SimpleLocation> regions, int scalar) {
-        Set<ContactRecord> toRemove = new HashSet<>();
-        Map<SimpleLocation, List<ContactRecord>> locationMap = new HashMap<>();
-        for (ContactRecord cr : initialPoints) {
-            SimpleLocation region = new SimpleLocation(cr.getBinX() / scalar, cr.getBinY() / scalar);
-            if (regions.contains(region)) {
-                if (locationMap.containsKey(region)) {
-                    locationMap.get(region).add(cr);
-                } else {
-                    List<ContactRecord> values = new ArrayList<>();
-                    values.add(cr);
-                    locationMap.put(region, values);
-                }
-            } else {
-                toRemove.add(cr);
-            }
-        }
-
-        nonMaxSuppressionInGroup(initialPoints, toRemove, locationMap);
-    }
 }
---
+++ src/cli/utils/sift/NMSUtils.java
@@ -0,0 +1,79 @@
+package cli.utils.sift;
+
+import javastraw.reader.block.ContactRecord;
+
+import java.util.*;
+
+public class NMSUtils {
+
+
+    public static void filterOutByOverlap(Set<ContactRecord> initialPoints, Set<SimpleLocation> regions, int scalar) {
+        Set<ContactRecord> toRemove = new HashSet<>();
+        Map<SimpleLocation, List<ContactRecord>> locationMap = new HashMap<>();
+        for (ContactRecord cr : initialPoints) {
+            SimpleLocation region = new SimpleLocation(cr.getBinX() / scalar, cr.getBinY() / scalar);
+            if (regions.contains(region)) {
+                if (locationMap.containsKey(region)) {
+                    locationMap.get(region).add(cr);
+                } else {
+                    List<ContactRecord> values = new ArrayList<>();
+                    values.add(cr);
+                    locationMap.put(region, values);
+                }
+            } else {
+                toRemove.add(cr);
+            }
+        }
+
+        nonMaxSuppressionInGroup(initialPoints, toRemove, locationMap);
+    }
+
+    public static void filterOutByOverlap(Set<ContactRecord> initialPoints, int scalar) {
+        Set<ContactRecord> toRemove = new HashSet<>();
+        Map<SimpleLocation, List<ContactRecord>> locationMap = new HashMap<>();
+        for (ContactRecord cr : initialPoints) {
+            SimpleLocation region = new SimpleLocation(cr.getBinX() / scalar, cr.getBinY() / scalar);
+            if (locationMap.containsKey(region)) {
+                locationMap.get(region).add(cr);
+            } else {
+                List<ContactRecord> values = new ArrayList<>();
+                values.add(cr);
+                locationMap.put(region, values);
+            }
+        }
+
+        nonMaxSuppressionInGroup(initialPoints, toRemove, locationMap);
+    }
+
+    public static void nonMaxSuppressionInGroup(Set<ContactRecord> initialPoints, Set<ContactRecord> toRemove, Map<SimpleLocation, List<ContactRecord>> locationMap) {
+        for (List<ContactRecord> overlaps : locationMap.values()) {
+            if (overlaps.size() > 1) {
+                float max = getMax(overlaps);
+                for (ContactRecord cr2 : overlaps) {
+                    if (cr2.getCounts() < max) {
+                        toRemove.add(cr2);
+                    }
+                }
+            }
+        }
+        initialPoints.removeAll(toRemove);
+        locationMap.clear();
+    }
+
+
+    public static float getMax(List<ContactRecord> records) {
+        float maxVal = records.get(0).getCounts();
+        for (ContactRecord cr : records) {
+            if (cr.getCounts() > maxVal) {
+                maxVal = cr.getCounts();
+            }
+        }
+        return maxVal;
+    }
+
+    public static boolean inRegions(ContactRecord cr, Set<SimpleLocation> regions, int scalar) {
+        SimpleLocation region = new SimpleLocation(cr.getBinX() / scalar, cr.getBinY() / scalar);
+        return regions.contains(region);
+    }
+
+}
