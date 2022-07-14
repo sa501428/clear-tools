67a61ab63a732a6e6fb6c0360e9c1e22fac42356
Muhammad Saad Shamim <sa501428@gmail.com>
2022-07-14 16:21:52 -0500

    update version

--- src/cli/Main.java
+++ src/cli/Main.java
@@ -5,7 +5,7 @@ import jargs.gnu.CmdLineParser;
 
 public class Main {
 
-    public static final String VERSION_NUM = "0.17.2";
+    public static final String VERSION_NUM = "0.18.0";
     public static final int DEFAULT_RESOLUTION = 5000;
     public static final int DEFAULT_CUTOFF = 500;
     public static final String DEFAULT_NORMALIZATION = "SCALE";
