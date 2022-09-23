6d373c221ee039fe95444849681f88651759b121
Muhammad Saad Shamim <sa501428@gmail.com>
2022-09-23 14:21:13 -0500

    use zscore for filtering

--- src/cli/utils/general/VectorCleaner.java
+++ src/cli/utils/general/VectorCleaner.java
@@ -1,5 +1,7 @@
 package cli.utils.general;
 
+import javastraw.expected.Welford;
+import javastraw.expected.Zscore;
 import org.apache.commons.math3.stat.descriptive.DescriptiveStatistics;
 
 public class VectorCleaner {
@@ -10,14 +12,20 @@ public class VectorCleaner {
             }
         }
 
-        double logThreshold = getLogLowerBound(vec);
+        double logThreshold = getLogLowerZscoreBound(vec, -2);
+        //double logThreshold = getLogLowerBound(vec);
         double lowerBound = Math.min(1, Math.exp(logThreshold));
         //double lowerBound = Math.min(1, getPercentile(vec, 16));
         for (int k = 0; k < vec.length; k++) {
-            if (vec[k] > 0 && vec[k] < lowerBound) {
+            if (vec[k] < lowerBound) {
                 vec[k] = Double.NaN;
             }
         }
+    }
+
+    private static double getLogLowerZscoreBound(double[] vec, float z) {
+        Zscore stats = createLogZscore(vec);
+        return stats.getZscore(z);
     }
 
     private static double getPercentile(double[] vec, int percentile) {
@@ -45,5 +53,15 @@ public class VectorCleaner {
             }
         }
         return stats;
+    }
+
+    private static Zscore createLogZscore(double[] vec) {
+        Welford welford = new Welford();
+        for (double v : vec) {
+            if (v > 0) {
+                welford.addValue(Math.log(v));
+            }
+        }
+        return welford.getZscore();
     }
 }
