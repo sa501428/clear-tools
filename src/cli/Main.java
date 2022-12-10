5d6b8308fd50d9543fff9ef15debe6f537e765e2
Muhammad Saad Shamim <sa501428@gmail.com>
2022-12-10 00:53:30 -0600

    update unwrapping

--- src/cli/Main.java
+++ src/cli/Main.java
@@ -5,7 +5,7 @@ import jargs.gnu.CmdLineParser;
 
 public class Main {
 
-    public static final String VERSION_NUM = "0.81.0";
+    public static final String VERSION_NUM = "0.82.0";
     public static boolean printVerboseComments = false;
 
     public static void printGeneralUsageAndExit(int exitCode, String cUsage) {
