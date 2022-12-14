5348faef0deb74ad65f68c257f6bd26b6a6e5f85
Muhammad Saad Shamim <sa501428@gmail.com>
2022-12-13 18:49:43 -0600

    bug fix for APA2 at hires

--- src/cli/clt/APA3.java
+++ src/cli/clt/APA3.java
@@ -1,13 +1,9 @@
 package cli.clt;
 
-import cli.utils.general.Utils;
-import javastraw.expected.ExpectedUtils;
 import javastraw.feature2D.Feature2D;
-import javastraw.reader.block.ContactRecord;
 import javastraw.reader.mzd.MatrixZoomData;
 
 import java.util.ArrayList;
-import java.util.Iterator;
 import java.util.List;
 import java.util.concurrent.atomic.AtomicInteger;
 
@@ -28,22 +24,5 @@ public class APA3 extends APA {
     protected void processLoopsForRegion(MatrixZoomData zd, List<Feature2D> loops,
                                          float[][] output, AtomicInteger currNumLoops,
                                          int numTotalLoops) {
-
-        Iterator<ContactRecord> it = ExpectedUtils.getIterator(zd, norm);
-        while (it.hasNext()) {
-            ContactRecord cr = it.next();
-            if (cr.getCounts() > 0) {
-
-            }
-        }
-
-        for (Feature2D loop : loops) {
-            int binXStart = (int) ((loop.getMidPt1() / resolution) - window);
-            int binYStart = (int) ((loop.getMidPt2() / resolution) - window);
-            Utils.addLocalBoundedRegion(output, zd, binXStart, binYStart, matrixWidthL, norm);
-            if (currNumLoops.incrementAndGet() % 100 == 0) {
-                System.out.print(((int) Math.floor((100.0 * currNumLoops.get()) / numTotalLoops)) + "% ");
-            }
-        }
     }
 }
