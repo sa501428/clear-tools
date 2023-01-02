477421f17c30c61dbeef92c4ecb99ac1bb61d9ec
Muhammad Saad Shamim <sa501428@gmail.com>
2023-01-02 15:39:53 -0600

    unwrap2

--- src/cli/clt/bedpe/UnWrap.java
+++ src/cli/clt/bedpe/UnWrap.java
@@ -18,7 +18,7 @@ import java.util.Map;
 
 public class UnWrap {
 
-    public static String usage = "unwrap[-filter][-legacy] [-r resolution] <genomeID> <loops.bedpe> <output_stem_>\n" +
+    public static String usage = "unwrap[2][-filter][-legacy] [-r resolution] <genomeID> <loops.bedpe> <output_stem_>\n" +
             "\t\tunwrap localizer output to proper hi-res inverted bounds list\n" +
             "\t\tlegacy mode will use the old method of unwrapping the combined anchors\n";
     private static final int numLists = 6;
@@ -38,23 +38,52 @@ public class UnWrap {
         boolean doFilter = command.contains("filter");
         boolean useLegacy = command.contains("legacy");
 
-        Feature2DList[] invertedLists = unwrap(loopList, doFilter, useLegacy);
-        invertedLists[0].exportFeatureList(new File(outFile + "unwrapped.anchors.bedpe"), false, Feature2DList.ListFormat.NA);
-        invertedLists[1].exportFeatureList(new File(outFile + "unwrapped.best.anchors.bedpe"), false, Feature2DList.ListFormat.NA);
-        invertedLists[2].exportFeatureList(new File(outFile + "unwrapped.local.bedpe"), false, Feature2DList.ListFormat.NA);
+        if (command.contains("2")) {
+            Feature2DList invertedList = unwrap2(loopList);
+            invertedList.exportFeatureList(new File(outFile + "unwrapped2.local.bedpe"), false, Feature2DList.ListFormat.NA);
+        } else {
+            Feature2DList[] invertedLists = unwrap(loopList, doFilter, useLegacy);
+            invertedLists[0].exportFeatureList(new File(outFile + "unwrapped.anchors.bedpe"), false, Feature2DList.ListFormat.NA);
+            invertedLists[1].exportFeatureList(new File(outFile + "unwrapped.best.anchors.bedpe"), false, Feature2DList.ListFormat.NA);
+            invertedLists[2].exportFeatureList(new File(outFile + "unwrapped.local.bedpe"), false, Feature2DList.ListFormat.NA);
 
-        if (doFilter) {
-            invertedLists[3].exportFeatureList(new File(outFile + "optimal.bedpe"), false, Feature2DList.ListFormat.NA);
-            invertedLists[4].exportFeatureList(new File(outFile + "suboptimal.anchor.bedpe"), false, Feature2DList.ListFormat.NA);
-            invertedLists[5].exportFeatureList(new File(outFile + "suboptimal.local.bedpe"), false, Feature2DList.ListFormat.NA);
+            if (doFilter) {
+                invertedLists[3].exportFeatureList(new File(outFile + "optimal.bedpe"), false, Feature2DList.ListFormat.NA);
+                invertedLists[4].exportFeatureList(new File(outFile + "suboptimal.anchor.bedpe"), false, Feature2DList.ListFormat.NA);
+                invertedLists[5].exportFeatureList(new File(outFile + "suboptimal.local.bedpe"), false, Feature2DList.ListFormat.NA);
+            }
         }
     }
 
-    private static Feature2DList[] unwrap(Feature2DList loopList, boolean doFilter, boolean useLegacy) {
-        Feature2DList[] unwrapped = new Feature2DList[numLists];
-        for (int k = 0; k < unwrapped.length; k++) {
-            unwrapped[k] = new Feature2DList();
+    private static Feature2DList unwrap2(Feature2DList loopList) {
+        Feature2DList unwrapped = new Feature2DList();
+
+        loopList.processLists((s, list) -> {
+            List<Feature2D> invLocalList = new ArrayList<>(list.size() / 2);
+
+            for (Feature2D feature2D : list) {
+                Feature2D inv = unwrapLocal(feature2D);
+                if (inv != null) {
+                    if (localXYNotNearDiagonal(inv)) {
+                        invLocalList.add(inv);
+                    }
+                }
+            }
+            unwrapped.addByKey(s, invLocalList);
+        });
+        return unwrapped;
+    }
+
+    private static Feature2DList[] initializeF2DArray(int n) {
+        Feature2DList[] array = new Feature2DList[n];
+        for (int k = 0; k < array.length; k++) {
+            array[k] = new Feature2DList();
         }
+        return array;
+    }
+
+    private static Feature2DList[] unwrap(Feature2DList loopList, boolean doFilter, boolean useLegacy) {
+        Feature2DList[] unwrapped = initializeF2DArray(numLists);
 
         loopList.processLists((s, list) -> {
             List<Feature2D> invAnchorsList = new ArrayList<>(list.size() / 2);
@@ -107,14 +136,26 @@ public class UnWrap {
         return false;
     }
 
+    private static boolean localXYNotNearDiagonal(Feature2D loop) {
+        long x = Long.parseLong(loop.getAttribute("localX"));
+        long y = Long.parseLong(loop.getAttribute("localY"));
+        int dist = (int) Math.min(Math.abs(x - y),
+                Math.abs(loop.getMidPt1() - loop.getMidPt2()));
+        return dist > 10000;
+    }
+
     private static Feature2D unwrapLocal(Feature2D feature2D) {
-        long start1 = Long.parseLong(feature2D.getAttribute("localX"));
-        long start2 = Long.parseLong(feature2D.getAttribute("localY"));
-        long end1 = start1 + resolution;
-        long end2 = start2 + resolution;
-        Map<String, String> attrs = new HashMap<>(feature2D.getAttributes());
-        return new Feature2D(Feature2D.FeatureType.PEAK, feature2D.getChr1(), start1, end1,
-                feature2D.getChr2(), start2, end2, Color.BLUE, attrs);
+        try {
+            long start1 = Long.parseLong(feature2D.getAttribute("localX"));
+            long start2 = Long.parseLong(feature2D.getAttribute("localY"));
+            long end1 = start1 + resolution;
+            long end2 = start2 + resolution;
+            Map<String, String> attrs = new HashMap<>(feature2D.getAttributes());
+            return new Feature2D(Feature2D.FeatureType.PEAK, feature2D.getChr1(), start1, end1,
+                    feature2D.getChr2(), start2, end2, Color.BLUE, attrs);
+        } catch (Exception ignored) {
+        }
+        return null;
     }
 
     private static int getDistanceBetweenAnchorsAndLocal(Feature2D loop) {
